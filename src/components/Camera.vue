49064644a0761e25d4cb3035621d6e1bd00eb4b3
JakobDrescher <jakobdrescher6@gmail.com>
2024-05-13 16:02:20 +0200

    fixed the first issue but now i cant take the width or height of the video

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,18 +1,36 @@
 <template>
   <h1>Camera</h1>
-  <video ref="video"></video>
+  <video ref="video" autoplay></video>
+  <button @click="takePicture();">Take Picture</button>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import {galleryStore} from '@/stores/gallery';
 
-let videoSrc;
-let video=ref(null);
+const gallery=galleryStore();
+const video=ref(null);
+let width;
+let height;
+
 navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
-  console.log(video.value);
-  video.srcObject = stream;
-  video.value.play();
+  video.value.srcObject = stream;
+
 }).catch((err) => {
   console.error(`An error occurred: ${err}`);
 });
+
+
+
+function takePicture(){
+  let picture= document.createElement('canvas');
+  let context = picture.getContext('2d');
+
+  picture.height=height;
+  picture.width=width;
+
+  context.drawImage(video.value,0,0,width,height);
+  console.log(picture.toDataURL('image/png'));
+  gallery.addPicture(picture.toDataURL('image/png'));
+}
 </script>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,10 @@
 <template>
 <h1>Gallery</h1>
+  <img v-for="picture in gallery.gallery" :src="picture">
 </template>
 
 <script setup>
+import {galleryStore} from "@/stores/gallery.js";
+
+let gallery=galleryStore();
 </script>
