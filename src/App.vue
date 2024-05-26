25474407054bb6fb0e3e273b22da960b8f5e2d9f
JakobDrescher <jakobdrescher6@gmail.com>
2024-05-26 15:36:38 +0200

    finished the excersise

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
 
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/camera">Head to the Camera</RouterLink>
-    <RouterLink to="/gallery">Head to the Gallery</RouterLink>
+  <nav class="container-fluid d-flex p-3 bg-dark justify-content-between">
+    <button class="btn btn-light col-4 col-sm-2" @click="$router.push('camera')">Camera</button>
+    <button class="btn btn-light col-4 col-sm-2" @click="$router.push('gallery')">Gallery</button>
   </nav>
-<main>
-  <RouterView></RouterView>
-</main>
+  <main class="container-fluid text-center justify-content-center">
+    <RouterView></RouterView>
+  </main>
 </template>
--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,35 +1,37 @@
 <template>
-  <h1>Camera</h1>
-  <video ref="video" autoplay></video>
-  <button @click="takePicture();">Take Picture</button>
+  <div class="row col-12 col-md-8 p-3 m-0 position-fixed top-50 start-50 translate-middle text-center justify-content-center g-4">
+    <h1>Camera</h1>
+    <video ref="video" autoplay class="object-fit-contain p-0 border border-secondary border-5 rounded shadow-none"></video>
+    <button @click="takePicture();" class="btn btn-primary col-sm-3">Take Picture</button>
+  </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
 import {galleryStore} from '@/stores/gallery';
 
-const gallery=galleryStore();
-const video=ref(null);
+const gallery = galleryStore();
+const video = ref(null);
 let width;
 let height;
 
 navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
   video.value.srcObject = stream;
-
+  height = stream.getVideoTracks()[0].getSettings().height;
+  width = stream.getVideoTracks()[0].getSettings().width;
 }).catch((err) => {
   console.error(`An error occurred: ${err}`);
 });
 
 
-
-function takePicture(){
-  let picture= document.createElement('canvas');
+function takePicture() {
+  let picture = document.createElement('canvas');
   let context = picture.getContext('2d');
 
-  picture.height=height;
-  picture.width=width;
+  picture.height = height;
+  picture.width = width;
 
-  context.drawImage(video.value,0,0,width,height);
+  context.drawImage(video.value, 0, 0, width, height);
   console.log(picture.toDataURL('image/png'));
   gallery.addPicture(picture.toDataURL('image/png'));
 }
